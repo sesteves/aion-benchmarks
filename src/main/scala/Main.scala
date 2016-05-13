6106930f9d30f6d36bcf71eb572b5ab233398aab
sesteves <sroesteves@gmail.com>
2016-05-13 20:39:51 +0100

    support for allowed lateness

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,10 +15,18 @@ object Main {
     val stream = env.fromElements((1,1),(2,1),(3,1),(4,1),(5,1))
       .assignAscendingTimestamps(p => System.currentTimeMillis)
 
-    stream.keyBy(0)
-      .timeWindow(Time.of(5, TimeUnit.MINUTES), Time.of(1, TimeUnit.MINUTES))
+    stream.keyBy(1)
+      .timeWindow(Time.of(5, TimeUnit.MINUTES))
       .sum(1)
       .print()
+
+    // The means that it will fire every 10 minutes (in processing time) until the end of the window (event time), and then every 5 minutes (processing time) for late elements up to 20 minutes late. In addition, previous elements are not discarded.
+      val trigger = EventTimeTriggerWithEarlyAndLateFiring.create()
+        .withEarlyFiringEvery(Time.minutes(10))
+        .withLateFiringEvery(Time.minutes(5))
+        .withAllowedLateness(Time.minutes(20))
+        .accumulating()
+
 
     env.execute()
   }
