6298ee2d25e6047759fb08d1e29e338b79b43cae
sesteves <sroesteves@gmail.com>
2016-05-16 02:11:07 +0100

    new trigger added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,9 @@ import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.triggers.Trigger.TriggerContext
+import org.apache.flink.streaming.api.windowing.triggers.{EventTimeTrigger, Trigger, TriggerResult}
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 
 object Main {
 
@@ -13,19 +16,39 @@ object Main {
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     val stream = env.fromElements((1,1),(2,1),(3,1),(4,1),(5,1))
-      .assignAscendingTimestamps(p => System.currentTimeMillis)
+      .assignAscendingTimestamps(p => System.currentTimeMillis + p._1 * 1000)
+
+    // The means that it will fire every 10 minutes (in processing time) until the end of the window (event time),
+    // and then every 5 minutes (processing time) for late elements up to 20 minutes late. In addition, previous
+    // elements are not discarded.
+    val trigger = EventTimeTriggerWithEarlyAndLateFiring.create()
+      .withEarlyFiringEvery(Time.seconds(1))
+      .withLateFiringEvery(Time.seconds(2))
+      .withAllowedLateness(Time.seconds(2))
+      .accumulating()
+
+    val trigger2 = new Trigger[Any, TimeWindow] {
+      var count = 0
+
+      override def onElement(t: Any, l: Long, w: TimeWindow, triggerContext: TriggerContext): TriggerResult = {
+        count += 1
+        if (count % 2 == 0) return TriggerResult.FIRE else return TriggerResult.CONTINUE
+      }
+
+      override def onProcessingTime(l: Long, w: TimeWindow, triggerContext: TriggerContext): TriggerResult =
+        return TriggerResult.FIRE
+
+      override def onEventTime(l: Long, w: TimeWindow, triggerContext: TriggerContext): TriggerResult =
+        return TriggerResult.FIRE
+    }
 
     stream.keyBy(1)
       .timeWindow(Time.of(5, TimeUnit.MINUTES))
+      .trigger(trigger2)
       .sum(1)
       .print()
 
-    // The means that it will fire every 10 minutes (in processing time) until the end of the window (event time), and then every 5 minutes (processing time) for late elements up to 20 minutes late. In addition, previous elements are not discarded.
-      val trigger = EventTimeTriggerWithEarlyAndLateFiring.create()
-        .withEarlyFiringEvery(Time.minutes(10))
-        .withLateFiringEvery(Time.minutes(5))
-        .withAllowedLateness(Time.minutes(20))
-        .accumulating()
+
 
 
     env.execute()
