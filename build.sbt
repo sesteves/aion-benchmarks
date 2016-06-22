6e3d9e5e110a2321fb63ff86e1036a271bb83ca2
sesteves <sroesteves@gmail.com>
2016-06-22 19:24:29 +0100

    collect memory stats

--- build.sbt
+++ build.sbt
@@ -3,6 +3,7 @@ name := "flink-test2"
 scalaVersion := "2.10.4"
 
 libraryDependencies ++= Seq(
+//  "org.fusesource" % "sigar" % "1.6.4"
 //  "org.apache.flink" % "flink-connector-kafka" % "1.0.2",
 //  "org.apache.flink" % "flink-streaming-scala_2.11" % "1.0.2",
 //  "org.apache.flink" % "flink-clients" % "0.10.2-hadoop1"
@@ -10,6 +11,9 @@ libraryDependencies ++= Seq(
 
 // unmanagedJars in Compile += file("lib/vertica_jdk_5.jar")
 
-//fork in run := true
+// fork in run := true
 
-//javaOptions in run ++= Seq("-Xms256M", "-Xmx256M", "-XX:MaxPermSize=256M")
+// javaOptions in run ++= Seq("-Xms256M", "-Xmx256M", "-XX:MaxPermSize=256M")
+
+// javaOptions in run += "-Djava.library.path=\"./native/\""
+
--- src/main/scala/CollectMemoryStats.scala
+++ src/main/scala/CollectMemoryStats.scala
@@ -1,28 +1,62 @@
+import javax.management.ObjectName
+import javax.management.openmbean.CompositeData
 import javax.management.remote.{JMXConnectorFactory, JMXServiceURL}
+
+import com.sun.tools.attach.VirtualMachine
+import org.hyperic.sigar.Sigar
+import org.hyperic.sigar.ptql.ProcessFinder
+
+import scala.collection.JavaConversions._
 
 /**
   * Created by sesteves on 21-06-2016.
+  *
+  * http://stackoverflow.com/questions/5552960/how-to-connect-to-a-java-program-on-localhost-jvm-using-jmx
   */
-
-//http://stackoverflow.com/questions/5552960/how-to-connect-to-a-java-program-on-localhost-jvm-using-jmx
 object CollectMemoryStats {
 
   def main(args: Array[String]): Unit = {
 
-    val host = "localhost"  // or some A.B.C.D
-    val port = 1234
-    val url = "service:jmx:rmi:///jndi/rmi://" + host + ":" + port + "/jmxrmi"
-    val serviceUrl = new JMXServiceURL(url)
-    val jmxConnector = JMXConnectorFactory.connect(serviceUrl, null)
+    val processName = "com.intellij.idea.Main"
+
+    val vmsDesc = VirtualMachine.list()
+    val vmDesc = vmsDesc.find(desc => {
+      println(s"name: ${desc.displayName()}, id: ${desc.id()}")
+      desc.displayName() == processName
+    })
+
+    if(vmDesc.isEmpty) return
 
     try {
-      val mbeanConn = jmxConnector.getMBeanServerConnection()
-      // now query to get the beans or whatever
-       val beanSet = mbeanConn.queryNames(null, null)
+      val vm = VirtualMachine.attach(vmDesc.get.id)
+      val props = vm.getAgentProperties
+      val connectorAddress = props.getProperty("com.sun.management.jmxremote.localConnectorAddress")
 
-    } finally {
-      jmxConnector.close();
+      val url = new JMXServiceURL(connectorAddress)
+      val connector = JMXConnectorFactory.connect(url)
+
+      val mbeanConn = connector.getMBeanServerConnection
+      for(i <- 1 to 10) {
+        val bean = mbeanConn.getAttribute(new ObjectName("java.lang:type=Memory"), "HeapMemoryUsage")
+         .asInstanceOf[CompositeData]
+        val values = bean.getAll(Array("used", "committed", "max"))
+        values.foreach(println)
+        Thread.sleep(1000)
+      }
+    } catch {
+      case ex: Exception => ex.printStackTrace
     }
+
+    val sigar = new Sigar
+    // val pids = ProcessFinder.find(sigar, s"CredName.User.eq=sesteves")
+    // pids.foreach(println)
+
+    val swap = sigar.getSwap
+    swap.getUsed
+
+
+
+
   }
 
 }
