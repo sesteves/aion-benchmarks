3e25798478895752c926de0e1a310b6af245c06e
sesteves <sroesteves@gmail.com>
2016-06-24 19:46:39 +0100

    minor

--- src/main/scala/CollectMemoryStats.scala
+++ src/main/scala/CollectMemoryStats.scala
@@ -5,7 +5,6 @@ import javax.management.remote.{JMXConnectorFactory, JMXServiceURL}
 
 import com.sun.tools.attach.{VirtualMachine, VirtualMachineDescriptor}
 import org.hyperic.sigar.Sigar
-import org.hyperic.sigar.ptql.ProcessFinder
 
 import scala.collection.JavaConversions._
 
@@ -32,48 +31,51 @@ object CollectMemoryStats {
     }
 
 
-    val fname = "stats-memory-%d.csv".format(System.currentTimeMillis)
-    val pw = new PrintWriter(new File(fname))
-    try {
-      pw.println("timestamp, used, commited, max, swap")
+    while(true) {
+      val fname = "stats-memory-%d.csv".format(System.currentTimeMillis)
+      val pw = new PrintWriter(new File(fname))
 
-      val vm = VirtualMachine.attach(vmDesc.get.id)
+      try {
+        pw.println("timestamp, used, commited, max, swap")
 
-      val PropertyConnectorAddress = "com.sun.management.jmxremote.localConnectorAddress"
-      val connectorAddress = {
-        if (vm.getAgentProperties.getProperty(PropertyConnectorAddress) == null) {
-          // no connector address, so we start the JMX agent
-          val agent = vm.getSystemProperties().getProperty("java.home") + File.separator + "lib" + File.separator +
-            "management-agent.jar"
-          vm.loadAgent(agent)
-          println(vm.getAgentProperties)
+        val vm = VirtualMachine.attach(vmDesc.get.id)
+
+        val PropertyConnectorAddress = "com.sun.management.jmxremote.localConnectorAddress"
+        val connectorAddress = {
+          if (vm.getAgentProperties.getProperty(PropertyConnectorAddress) == null) {
+            // no connector address, so we start the JMX agent
+            val agent = vm.getSystemProperties().getProperty("java.home") + File.separator + "lib" + File.separator +
+              "management-agent.jar"
+            vm.loadAgent(agent)
+            println(vm.getAgentProperties)
+          }
+          vm.getAgentProperties.getProperty(PropertyConnectorAddress)
         }
-        vm.getAgentProperties.getProperty(PropertyConnectorAddress)
+
+        val url = new JMXServiceURL(connectorAddress)
+        val connector = JMXConnectorFactory.connect(url)
+
+        val mbeanConn = connector.getMBeanServerConnection
+        while (true) {
+          val bean = mbeanConn.getAttribute(new ObjectName("java.lang:type=Memory"), "HeapMemoryUsage")
+            .asInstanceOf[CompositeData]
+          val values = bean.getAll(Array("used", "committed", "max"))
+
+          val sigar = new Sigar
+          // val pids = ProcessFinder.find(sigar, s"CredName.User.eq=sesteves")
+          // pids.foreach(println)
+          val swap = sigar.getSwap
+
+          val stat = "%d,%d,%d,%d,%d".format(System.currentTimeMillis(), values(0), values(1), values(2), swap.getUsed)
+          pw.println(stat)
+
+          Thread.sleep(1000)
+        }
+      } catch {
+        case ex: Exception => ex.printStackTrace
+      } finally {
+        pw.close
       }
-
-      val url = new JMXServiceURL(connectorAddress)
-      val connector = JMXConnectorFactory.connect(url)
-
-      val mbeanConn = connector.getMBeanServerConnection
-      while(true) {
-        val bean = mbeanConn.getAttribute(new ObjectName("java.lang:type=Memory"), "HeapMemoryUsage")
-         .asInstanceOf[CompositeData]
-        val values = bean.getAll(Array("used", "committed", "max"))
-
-        val sigar = new Sigar
-        // val pids = ProcessFinder.find(sigar, s"CredName.User.eq=sesteves")
-        // pids.foreach(println)
-        val swap = sigar.getSwap
-
-        val stat = "%d,%d,%d,%d,%d".format(System.currentTimeMillis(), values(0), values(1), values(2), swap.getUsed)
-        pw.println(stat)
-
-        Thread.sleep(1000)
-      }
-    } catch {
-      case ex: Exception => ex.printStackTrace
-    } finally {
-      pw.close
     }
 
   }
