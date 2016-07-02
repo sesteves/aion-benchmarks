684c23a434c6292b19d8d3162445ab2063f23aec
sesteves <sroesteves@gmail.com>
2016-07-02 20:14:25 +0100

    fork process

---
+++ src/main/scala/DataDump.scala
@@ -0,0 +1,21 @@
+import java.io.{File, PrintWriter}
+import java.net.{InetAddress, Socket}
+
+import scala.io.BufferedSource
+
+/**
+  * Created by Sergio on 02/07/2016.
+  */
+object DataDump {
+
+  def main(args: Array[String]): Unit = {
+
+    val socket = new Socket(InetAddress.getByName("localhost"), 9990)
+    val pw = new PrintWriter(new File("records.txt" ))
+    lazy val in = new BufferedSource(socket.getInputStream()).getLines()
+    in.foreach(pw.println)
+
+    pw.close()
+    socket.close()
+  }
+}
