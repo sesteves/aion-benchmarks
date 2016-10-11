96df2f6f9c7e5309570c76344e5fd44f7a1c0b49
sesteves <sroesteves@gmail.com>
2016-10-12 00:05:50 +0100

    made data generator unbounded

--- src/main/scala/DataGenerator.scala
+++ src/main/scala/DataGenerator.scala
@@ -8,12 +8,12 @@ object DataGenerator {
 
   def main(args: Array[String]): Unit = {
 
-//    if (args.length != 3) {
-//      System.err.println("Usage: DataGenerator <port> <nrecords> <sleepMillis>")
-//      System.exit(1)
-//    }
+    //    if (args.length != 3) {
+    //      System.err.println("Usage: DataGenerator <port> <nrecords> <sleepMillis>")
+    //      System.exit(1)
+    //    }
     // Parse the arguments using a pattern match
-//    val (port, nrecords, sleepMillis) = (args(0).toInt, args(1).toInt, args(2).toLong)
+    //    val (port, nrecords, sleepMillis) = (args(0).toInt, args(1).toInt, args(2).toLong)
 
     val (port, sleepMillis) = (9990, 10)
 
@@ -22,17 +22,18 @@ object DataGenerator {
 
     // val nrecords = Seq(100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000)
     // val nrecords = Seq(1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000, 1000000)
-    val nrecords = Seq(1200000, 1400000, 1600000, 1800000)
+    // val nrecords = Seq(1200000, 1400000, 1600000, 1800000)
 
-    while(true) {
-      for(i <- 0 to nrecords.size - 1) {
-        val socket = serverSocket.accept()
-        println("Got a new connection")
+    while (true) {
+      // for(i <- 0 to nrecords.size - 1) {
+      val socket = serverSocket.accept()
+      println("Got a new connection")
 
-        val out = new PrintWriter(socket.getOutputStream)
-        try {
-
-          (1 to nrecords(i)).foreach(j => out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
+      val out = new PrintWriter(socket.getOutputStream)
+      try {
+        var i = 0
+        while (true) {
+          out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
             "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
             "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
             "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
@@ -54,20 +55,20 @@ object DataGenerator {
             "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
             "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
             "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" +
-            " 1 " + j.toString))
-          out.flush
-
-          println(s"${nrecords(i)} lines sent")
-        } catch {
-          case ex: IOException => ex.printStackTrace()
-        } finally {
-          out.close()
-          socket.close()
+            " 1 " + i.toString)
+          i += 1
+          if(i == 1000000) i = 0
+          if(i % 100000 == 0) out.flush
         }
+
+        // println(s"${nrecords(i)} lines sent")
+      } catch {
+        case ex: IOException => ex.printStackTrace()
+      } finally {
+        out.close()
+        socket.close()
       }
+      // }
     }
-
-
   }
-
 }
