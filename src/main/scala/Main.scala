64234793a7200d5627102d22b0fdc7f00aa2a83f
sesteves <sroesteves@gmail.com>
2016-11-13 04:02:00 +0000

    minor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -233,14 +233,15 @@ object Main {
       collector.collect(iterator.reduce((p1, p2) => (p1._1, p1._2 + p2._2)))
     def fairlyComplexFunction = (key: Tuple, timeWindow: TimeWindow, iterator: Iterable[(String, Int)],
                                  collector: Collector[(String, Int)]) => {
-
       val startTick = System.currentTimeMillis()
-      collector.collect(iterator.reduce((p1, p2) => (p1._1, p1._2 + p2._2)))
+      // iterator shall never be called more than once
+      val result = iterator.reduce((p1, p2) => (p1._1, p1._2 + p2._2))
+      collector.collect(result)
       val endTick = System.currentTimeMillis()
-      println("### Iterator size: " + iterator.size + ", window: " + timeWindow)
+      println("### Iterator size: " + result._2 + ", window: " + timeWindow)
 
       val pw = new PrintWriter(new FileOutputStream(new File(computeStartFName), true), true)
-      pw.println(s"${timeWindow.maxTimestamp()},$startTick,$endTick,${iterator.size}")
+      pw.println(s"${timeWindow.maxTimestamp()},$startTick,$endTick,${result._2}")
     }
     def complexFunction = (key: Tuple, timeWindow: TimeWindow, iterator: Iterable[(String, Int)],
                                  collector: Collector[(String, Int)]) =>
